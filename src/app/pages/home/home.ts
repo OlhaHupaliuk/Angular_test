fc94dadd6e2135ce2e4032b6da22776f3ab0127c
OlhaHupaliuk <olha.hupaliuk@lnu.edu.ua>
2025-08-11 13:59:39 +0300

    feat(search-by-city): add filter, form, json server

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -11,7 +11,27 @@ import { HousingLocationInfo } from '../../housinglocation.model';
 export class HomeComponent {
   housingLocationList: HousingLocationInfo[] = [];
   housingService: HousingService = inject(HousingService);
-  constructor(){
-    this.housingLocationList = this.housingService.getAllHousingLocations();
+  filteredLocationList: HousingLocationInfo[] = [];
+  
+  constructor() {
+    this.housingService
+    .getAllHousingLocations()
+    .then((housingLocationList: HousingLocationInfo[]) => {
+      this.housingLocationList = housingLocationList;
+      this.filteredLocationList = housingLocationList;
+    })
+
+    
+  }
+
+  filterResults(text: string) {
+    console.log(text)
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+    this.filteredLocationList = this.housingLocationList.filter((housingLocation) =>
+      housingLocation?.city.toLowerCase().includes(text.toLowerCase()), 
+    );
   }
 }
